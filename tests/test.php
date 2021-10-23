7e558d040d77aa90b6b2fd2692394419ffc0b929
读心印 <839024615@qq.com>
2021-10-23 11:05:10 +0800

    v2

--- tests/test.php
+++ tests/test.php
@@ -9,7 +9,7 @@ use Php127\Douyin\Factory;
 //Douyin::url("https://v.douyin.com/eeYy4Yo/");
 
 
-$app = Factory::douyin("https://v.douyin.com/eeYy4Yo/");
+$app = Factory::douyin("https://v.douyin.com/RJAGV9g/");
 
 $data = $app->getData();
 
